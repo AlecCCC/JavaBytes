21227c9d8b27c6535e314e44685f0dd5c3b00d50
Alec <alecjames1996@aol.com>
2026-03-19 23:41:49 -0400

    Linked List
    Remove duplicate method done

--- 10-LinkedList/src/LinkedList.java
+++ 10-LinkedList/src/LinkedList.java
@@ -257,7 +257,18 @@ public class LinkedList {
 
         Node current = head;
 
-
+        while(current != null) {
+            Node runner = current;
+            while (runner.next != null) {
+                if (runner.next.value == current.value) {
+                    runner.next = runner.next.next;
+                    length = length - 1;
+                } else {
+                    runner = runner.next;
+                }
+            }
+            current = current.next;
+        }
     }
 
     public void getHead() {
--- 10-LinkedList/src/Main.java
+++ 10-LinkedList/src/Main.java
@@ -6,10 +6,18 @@ public class Main {
         linkedList.prepend(20);  //prepend
         linkedList.append(6); // add Node
         linkedList.append(20); // add Node
+        linkedList.append(6);
 
         linkedList.printList();
         System.out.println("\n");
         System.out.println(linkedList.kthFromEnd(1).value);
+
+        System.out.println("\n");
+        linkedList.removeDuplicate();
+        linkedList.printList();
+
+
+
 
 
     }
