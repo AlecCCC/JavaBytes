873be4ca66799aa4bb8d46fe8ae987eeece8cb19
Alec <alecjames1996@aol.com>
2026-03-20 22:43:58 -0400

    Linked List
    Added partitionList method

--- 10-LinkedList/src/Main.java
+++ 10-LinkedList/src/Main.java
@@ -7,6 +7,18 @@ public class Main {
         linkedList.append(20);
         linkedList.append(6);
 
+        LinkedList linkedList1 = new LinkedList(3);
+
+        for (int i = 0; i < 10; i++) {
+            linkedList1.append((int)(Math.random() * 20) + 1);
+        }
+
+        linkedList1.printList();
+        System.out.println();
+        linkedList1.partitionList(6);
+        linkedList1.printList();
+
+
 
 
     }
