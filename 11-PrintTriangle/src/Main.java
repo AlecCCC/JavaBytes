be7dd11236e2ba498a1d7d50bfbab44c3e48f0fe
Alec <alecjames1996@aol.com>
2026-03-21 21:47:26 -0400

    Printing Triangle
    Changed to method

--- 11-PrintTriangle/src/Main.java
+++ 11-PrintTriangle/src/Main.java
@@ -7,23 +7,28 @@ public class Main {
         System.out.println("Enter number of rows: ");
         int rows = scanner.nextInt();
 
-        System.out.println();
+        rightTriangle(5);
 
-        for (int i = 0; i < rows; i++) {
+
+
+    }
+
+    public static void rightTriangle(int rows){
+
+        for (int i = 0; i <= rows; i++) {
 
             for (int j = 0; j < rows; j++) {
                 System.out.print("");
             }
 
-            for (int j = 1; j <i; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
 
             System.out.println();
 
-
-
         }
 
     }
+
 }
