d5f6059ba192912984d18092a40fb6d2a106a15f
Alec <alecjames1996@aol.com>
2026-03-22 19:22:16 -0400

    Number pyramid
    upside down number triangle

--- 12-NumberLoop/src/Main.java
+++ 12-NumberLoop/src/Main.java
@@ -3,12 +3,26 @@ public class Main {
 
         numbers(4);
         System.out.println();
+        upsideDownNumbers(4);
 
 
     }
     public static void numbers(int number) {
 
         for (int i = 1; i <= number; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+
+    }
+
+    public static void upsideDownNumbers(int number) {
+
+        for (int i = number; i >= 1; i--) {
 
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
