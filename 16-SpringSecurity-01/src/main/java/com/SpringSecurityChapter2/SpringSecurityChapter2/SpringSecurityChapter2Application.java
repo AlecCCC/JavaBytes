e0717732241f93a6da1cab0192cf1f0b05fe914e
Alec <alecjames1996@aol.com>
2026-03-26 15:26:41 -0400

    Spring Security

---
+++ 16-SpringSecurity-01/src/main/java/com/SpringSecurityChapter2/SpringSecurityChapter2/SpringSecurityChapter2Application.java
@@ -0,0 +1,13 @@
+package com.SpringSecurityChapter2.SpringSecurityChapter2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityChapter2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityChapter2Application.class, args);
+	}
+
+}
---
+++ 16-SpringSecurity-01/src/test/java/com/SpringSecurityChapter2/SpringSecurityChapter2/SpringSecurityChapter2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringSecurityChapter2.SpringSecurityChapter2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityChapter2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
