1cb798c3ca52a431572346fbc13b5cd0c79c51a4
Alec <alecjames1996@aol.com>
2026-04-26 00:06:13 -0400

    Shuffle Cards

--- 17-Cards/src/main/java/org/example/Deck.java
+++ 17-Cards/src/main/java/org/example/Deck.java
@@ -1,11 +1,14 @@
 package org.example;
 
+
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 
 public class Deck {
 
-   private List<Card> cards;
+    private final Random random = new Random();
+
+   private ArrayList<Card> cards;
 
    public Deck() {
        cards = new ArrayList<>();
@@ -16,7 +19,28 @@ public class Deck {
        }
     }
 
-    public List<Card> getCards() {
+    public void printDeck() {
+        for (Card card : cards) {
+            System.out.println(card);
+        }
+    }
+
+    public void shuffle() {
+        System.out.println(cards.size());
+       for (int i = cards.size()-1; i > 0; i--) {
+
+           int randomi = random.nextInt(i+1);
+
+           Card temp = cards.get(i);
+           cards.set(i, cards.get(randomi));
+           cards.set(randomi, temp);
+       }
+    }
+
+
+
+
+    public ArrayList<Card> getCards() {
         return cards;
     }
 }
