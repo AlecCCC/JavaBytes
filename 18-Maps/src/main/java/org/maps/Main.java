94f276216c6c4111f8ddd1229c3e808b093e5abf
Alec <alecjames1996@aol.com>
2026-04-29 22:47:59 -0400

    Hashmap bull problem

--- 18-Maps/src/main/java/org/maps/Main.java
+++ 18-Maps/src/main/java/org/maps/Main.java
@@ -7,36 +7,59 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
 
-        Map<String, Integer> empIds = new HashMap<>();
+//        Map<String, Integer> empIds = new HashMap<>();
+//
+//        empIds.put("Alec", 1230);
+//        empIds.put("Donald", 0326);
+//        empIds.put("Mike", 9716);
+//
+//        System.out.println(empIds);
+//
+//        System.out.println(empIds.get("Alec"));
+//
+//        System.out.println(empIds.containsKey("Alec"));
+//        System.out.println(empIds.containsValue(1230));
+//
+//        //Can overwrite Keys if we put on the same key
+//        empIds.put("Alec", 6767);
+//        System.out.println(empIds);
+//
+//        //replace only does something if the key exists
+//        empIds.replace("Dave", 9999);
+//
+//        //IF key value doesnt exist, it will add the key value
+//        empIds.putIfAbsent("Steve",2222);
+//        System.out.println(empIds);
+//
+//        empIds.remove("Steve");
+//        System.out.println(empIds);
 
-        empIds.put("Alec", 1230);
-        empIds.put("Donald", 0326);
-        empIds.put("Mike", 9716);
+        HashMap<String,String> bully = new HashMap<>();
 
-        System.out.println(empIds);
+        System.out.println(mapBully(bully));
 
-        System.out.println(empIds.get("Alec"));
+        bully.put("a", "candy");
+        bully.put("b", "");
 
-        System.out.println(empIds.containsKey("Alec"));
-        System.out.println(empIds.containsValue(1230));
-
-        //Can overwrite Keys if we put on the same key
-        empIds.put("Alec", 6767);
-        System.out.println(empIds);
-
-        //replace only does something if the key exists
-        empIds.replace("Dave", 9999);
-
-        //IF key value doesnt exist, it will add the key value
-        empIds.putIfAbsent("Steve",2222);
-        System.out.println(empIds);
-
-        empIds.remove("Steve");
-        System.out.println(empIds);
+        System.out.println(mapBully(bully));
 
 
 
 
 
     }
+
+
+
+    public static Map<String,String> mapBully(Map<String, String> map) {
+
+        if (map.containsKey("a")) {
+            map.put("b", map.get("a"));
+            map.put("a", "");
+        }
+
+        return map;
+    }
+
+
 }
