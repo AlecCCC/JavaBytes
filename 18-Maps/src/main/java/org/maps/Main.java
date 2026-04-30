161378e05a79b9e59ea78e550787c4c8b9574a78
Alec <alecjames1996@aol.com>
2026-04-29 22:22:11 -0400

    Hasmap practice startout done

--- 18-Maps/src/main/java/org/maps/Main.java
+++ 18-Maps/src/main/java/org/maps/Main.java
@@ -17,6 +17,23 @@ public class Main {
 
         System.out.println(empIds.get("Alec"));
 
+        System.out.println(empIds.containsKey("Alec"));
+        System.out.println(empIds.containsValue(1230));
+
+        //Can overwrite Keys if we put on the same key
+        empIds.put("Alec", 6767);
+        System.out.println(empIds);
+
+        //replace only does something if the key exists
+        empIds.replace("Dave", 9999);
+
+        //IF key value doesnt exist, it will add the key value
+        empIds.putIfAbsent("Steve",2222);
+        System.out.println(empIds);
+
+        empIds.remove("Steve");
+        System.out.println(empIds);
+
 
 
 
