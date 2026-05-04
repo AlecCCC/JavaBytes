54a16c879d95a029b4b480c4024ee3ba7fa2e2fb
Alec <alecjames1996@aol.com>
2026-05-04 19:10:12 -0400

    Maptopping2 problem

--- 18-Maps/src/main/java/org/maps/Main.java
+++ 18-Maps/src/main/java/org/maps/Main.java
@@ -101,4 +101,18 @@ public class Main {
 
     }
 
+    public static Map<String, String> topping2(Map<String, String> map) {
+
+        if (map.containsKey("ice cream")) {
+            map.put("yogurt", map.get("ice cream"));
+        }
+
+        if (map.containsKey("spinach")) {
+            map.put("spinach", "nuts");
+        }
+        return map;
+    }
+
+
+
 }
