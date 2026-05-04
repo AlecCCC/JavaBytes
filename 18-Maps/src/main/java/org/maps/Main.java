898ccd5212e8338635226b64ce1a33b88bcfaf8a
Alec Martin <amartin20@bw.edu>
2026-05-03 21:48:50 -0400

    Practice

--- 18-Maps/src/main/java/org/maps/Main.java
+++ 18-Maps/src/main/java/org/maps/Main.java
@@ -7,32 +7,32 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
 
-//        Map<String, Integer> empIds = new HashMap<>();
-//
-//        empIds.put("Alec", 1230);
-//        empIds.put("Donald", 0326);
-//        empIds.put("Mike", 9716);
-//
-//        System.out.println(empIds);
-//
-//        System.out.println(empIds.get("Alec"));
-//
-//        System.out.println(empIds.containsKey("Alec"));
-//        System.out.println(empIds.containsValue(1230));
-//
-//        //Can overwrite Keys if we put on the same key
-//        empIds.put("Alec", 6767);
-//        System.out.println(empIds);
-//
-//        //replace only does something if the key exists
-//        empIds.replace("Dave", 9999);
-//
-//        //IF key value doesnt exist, it will add the key value
-//        empIds.putIfAbsent("Steve",2222);
-//        System.out.println(empIds);
-//
-//        empIds.remove("Steve");
-//        System.out.println(empIds);
+        Map<String, Integer> empIds = new HashMap<>();
+
+        empIds.put("Alec", 1230);
+        empIds.put("Donald", 0326);
+        empIds.put("Mike", 9716);
+
+        System.out.println(empIds);
+
+        System.out.println(empIds.get("Alec"));
+
+        System.out.println(empIds.containsKey("Alec"));
+        System.out.println(empIds.containsValue(1230));
+
+        //Can overwrite Keys if we put on the same key
+        empIds.put("Alec", 6767);
+        System.out.println(empIds);
+
+        //replace only does something if the key exists
+        empIds.replace("Dave", 9999);
+
+        //IF key value doesnt exist, it will add the key value
+        empIds.putIfAbsent("Steve",2222);
+        System.out.println(empIds);
+
+        empIds.remove("Steve");
+        System.out.println(empIds);
 
         HashMap<String,String> bully = new HashMap<>();
 
@@ -84,6 +84,18 @@ public class Main {
         if (map.containsKey("a") && map.containsKey("b")) {
             map.put("ab", map.get("a") + map.get("b"));
         }
+
+        return map;
+
+    }
+
+    public static Map<String, String> topping1(Map<String, String> map) {
+
+        if (map.containsKey("ice cream")) {
+            map.put("ice cream", "cherry");
+        }
+
+        map.put("bread", "butter");
 
         return map;
 
