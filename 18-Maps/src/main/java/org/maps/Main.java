e66edd16af3f69f8b8f53d293e3c7f07d9227dbb
Alec <alecjames1996@aol.com>
2026-05-06 23:48:17 -0400

    Longer Map Size

--- 18-Maps/src/main/java/org/maps/Main.java
+++ 18-Maps/src/main/java/org/maps/Main.java
@@ -113,6 +113,27 @@ public class Main {
         return map;
     }
 
+    public static Map<String, String> longerSize(Map<String, String> map) {
+
+        if (map.containsKey("a") && map.containsKey("b")) {
+
+            int aString = map.get("a").length();
+            int bString = map.get("b").length();
+
+            if (aString > bString) {
+                map.put("c", map.get("a"));
+            } else if (bString > aString) {
+                map.put("c", map.get("b"));
+            } else {
+                map.put("a", "");
+                map.put("b", "");
+            }
+
+
+        }
+        return map;
+    }
+
 
 
 }
